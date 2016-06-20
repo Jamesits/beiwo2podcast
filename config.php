bc303d823fa17575dccdf2510292132f9179915b
James Swineson <jamesswineson@gmail.com>
2016-06-20 15:35:44 +0800

    revert to manual URL settings

--- config.php
+++ config.php
@@ -8,16 +8,8 @@
 $version = '1.5';
 $php_curl_class_version = '3.6.7';
 
-// 安装目录
-// 手工设置
-// $install_path = 'http://lab.swineson.me/lab/beiwo2podcast/';
-function endsWith($haystack, $needle) {
-    // http://stackoverflow.com/a/10473026/2646069
-    // search forward starting from end minus needle length characters
-    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
-}
-$install_path = (isset($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-if (endsWith($install_path, ".php")) $install_path = dirname($install_path) . '/';
+// 安装目录（包括结尾斜杠）
+$install_path = 'http://lab.swineson.me/lab/beiwo2podcast/';
 // 是否显示那个测试服务器的提示
 $isDevelopingServer = true;
 // 默认一次读取的单集数
